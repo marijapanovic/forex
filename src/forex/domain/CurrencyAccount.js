9c6dfeb6922b987291e9d90d9cfbbd03b7aaf9df
Marija Prokic <marijapanovic@yahoo.com>
2020-05-24 18:19:45 +0200

    Create currency accounts in user account on user account creation

---
+++ src/forex/domain/CurrencyAccount.js
@@ -0,0 +1,7 @@
+class CurrencyAccount {
+    constructor(currency) {
+        this.currency = currency;
+        this.amount = 0.0;
+    }
+}
+export { CurrencyAccount };
--- src/forex/domain/UserAccount.js
+++ src/forex/domain/UserAccount.js
@@ -1,8 +1,24 @@
+import { CurrencyAccount } from "./CurrencyAccount";
+
 class UserAccount {
     constructor(user, primaryCurrency, secondaryCurrencies) {
         this.user = user;
         this.primaryCurrency = primaryCurrency;
         this.secondaryCurrencies = secondaryCurrencies;
+        this.currencyAccounts = {};
+        this.createCurrencyAccount(primaryCurrency);
+        for (let i=0; i < secondaryCurrencies.length; i++) {
+            let secondaryCurrency = secondaryCurrencies[i];
+            this.createCurrencyAccount(secondaryCurrency);
+        }
+    }
+
+    createCurrencyAccount(currency) {
+        if (currency in this.currencyAccounts) {
+            throw new Error("User " + this.user.username
+                + " already has account in currency " + currency);
+        }
+        this.currencyAccounts[currency] = new CurrencyAccount(currency);
     }
 }
 export { UserAccount };
